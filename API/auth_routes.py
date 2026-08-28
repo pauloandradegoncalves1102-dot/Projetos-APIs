606871fc8b392ffe1b5207ed47aefa0b8cefbc50
Paulo Andrade <pauloandradegoncalves1102@gmail.com>
2026-08-28 14:13:07 -0300

    Delete API directory

--- API/auth_routes.py
+++
@@ -1,4 +0,0 @@
-from fastapi import APIRouter
-
-auth_router = APIRouter(prefix="/auth", tags=["auth"])
-
--- API/order_routes.py
+++
@@ -1,3 +0,0 @@
-from fastapi import APIRouter
-
-order_router = APIRouter(prefix="/pedidos", tags=["Pedidos"])
